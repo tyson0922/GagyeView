d4b008707bb33186e794d3c71b05dc16d359b812
tyson0922 <taeseung92l@gmail.com>
2025-04-04 18:47:50 +0900

    index page, app bar.css, main-section.css started. need to finish index page and then go to userRegForm.html

---
+++ src/main/java/kopo/gagyeview/controller/MainController.java
@@ -0,0 +1,21 @@
+package kopo.gagyeview.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@RequiredArgsConstructor
+@Controller
+public class MainController {
+
+    @GetMapping(value="/")
+    public String mainPage(){
+
+        log.info("{}.mainPage Start!", this.getClass().getName());
+        log.info("{}.mainPage End!", this.getClass().getName());
+        return "index";
+    }
+
+}
